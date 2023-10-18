e6154159fd5838681374b3ed67902376a0a71f40
Alvin Patrick Bilog <balvinpatrick@yahoo.com>
2023-10-18 14:01:52 -0500

    added gridfs and multer to handle image upload in the backend

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -18,6 +18,7 @@ export default postRouter;
 async function createPost(req: Request, res: Response, next: NextFunction) {
   try {
     const requestBody = req.body as CreatePostType;
+    console.log('requestBody', requestBody);
     const postData = postValidator.createPostValidator.parse(requestBody);
     const createdPost = await postServices.create(postData);
     res.status(200).json({ success: true, data: createdPost });
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import router from './routes';
 import errorHandler from './middlewares/error-handler';
 import connectDb from './configs/db.config';
 import mongoose from 'mongoose';
+import { GridFsStorage } from 'multer-gridfs-storage';
+import Grid from 'gridfs-stream';
+import multer from 'multer';
 
 dotenv.config();
 
@@ -22,6 +25,58 @@ app.use(cors());
 app.use(cookieParser()); // Use cookie-parser middleware
 
 app.use('/api/v1', router);
+
+// GridFS Configuration
+let gfs: any;
+
+mongoose.connection.once('open', () => {
+  // Initialize GridFS stream
+  gfs = Grid(mongoose.connection.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+const storage = new GridFsStorage({
+  url: process.env.MONGO_URI!, // Replace with your MongoDB connection string
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      const filename = file.originalname;
+      const fileInfo = {
+        filename: filename,
+        bucketName: 'uploads',
+      };
+      resolve(fileInfo);
+    });
+  },
+});
+
+const upload = multer({ storage });
+
+// Upload endpoint
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ file: req.file });
+});
+
+// Get image endpoint
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne(
+    { filename: req.params.filename },
+    (err: any, file: any) => {
+      if (!file || file.length === 0) {
+        return res.status(404).json({ err: 'No file exists' });
+      }
+
+      if (
+        file.contentType === 'image/jpeg' ||
+        file.contentType === 'image/png'
+      ) {
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      } else {
+        res.status(404).json({ err: 'Not an image' });
+      }
+    }
+  );
+});
 
 //error handler
 app.use(errorHandler);
